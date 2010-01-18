c1784147b79484cf0373641ffd7bf6dbcb653baf
aVik <avikez@gmail.ru>
2010-01-18 12:48:48 +0000

    Modifed bin/update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 #
-date "+%H:%M" >> /usr/local/inetstat/var/update.log
+date >> /usr/local/inetstat/var/update.log
 su -l root -c "cd /usr/local/inetstat/ && git pull >> /usr/local/inetstat/var/update.log"
