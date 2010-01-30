6559c2e2d2eca0affee1bd619abf89d2bf967578
aVikez <avikez@gmail.com>
2010-01-30 23:59:08 +0000

    Fixed news from git hub

--- www/news.php
+++ www/news.php
@@ -1,16 +1,16 @@
 <?php
 $news_file = "http://github.com/api/v2/xml/commits/list/avik/InetStat/master/";
 $news_count=0;
-
+$news_count_current=1;
 function print_news($news_name,$news_mail,$news_url,$news_id,$news_date,$news_message)
 {
-$news_date[10]=" ";
 echo "<b>".$news_date."</b><a href=mailto:".$news_mail.">".$news_mail."</a><br>";
 echo "<b>Message:</b><a href=".$news_url.">".$news_message."</a><p>";
 }
 
 function characterData($news_parser, $news_data) 
 {
+	global $news_count_current;
 	global $news_count;
 	global $news_name;
 	global $news_mail;
@@ -18,32 +18,64 @@ function characterData($news_parser, $news_data)
 	global $news_id;
 	global $news_date;
 	global $news_message;
-	switch ($news_count) 
-	{
-		case 15:
-			$news_url = $news_data;
-			break;
-		case 17:
-			$news_id = $news_data;
-			break;
-		case 19:
-			$news_date=substr($news_data, 0, 10);
-			break;
-		case 23:
-			$news_message = $news_data;
-			break;
-		case 28:
-			$news_name = $news_data;
-			break;
-		case 31:
-			$news_mail = $news_data;
-			break;
-		case 34:
-			$news_count=0;
-			print_news($news_name,$news_mail,$news_url,$news_id,$news_date,$news_message);
-			break;
+	if ($news_count_current==0) {
+		switch ($news_count) 
+		{
+			case 17:
+				$news_url = $news_data;
+				break;
+			case 19:
+				$news_id = $news_data;
+				break;
+			case 21:
+				$news_date=substr($news_data, 0, 10);
+				break;
+			case 25:
+				$news_message = $news_data;
+				break;
+			case 30:
+				$news_name = $news_data;
+				break;
+			case 33:
+				$news_mail = $news_data;
+				break;
+			case 34:
+				$news_count_current=0;
+				$news_count=0;
+				print_news($news_name,$news_mail,$news_url,$news_id,$news_date,$news_message);
+				break;
+		}
 	}
-	$news_count++;
+	else
+	{
+		switch ($news_count) 
+		{
+			case 16:
+				$news_url = $news_data;
+				break;
+			case 19:
+				$news_id = $news_data;
+				break;
+			case 22:
+				$news_date=substr($news_data, 0, 10);
+				break;
+			case 24:
+				$news_message = $news_data;
+				break;
+			case 30:
+				$news_name = $news_data;
+				break;
+			case 33:
+				$news_mail = $news_data;
+				break;
+			case 34:
+				$news_count_current=0;
+				$news_count=0;
+				print_news($news_name,$news_mail,$news_url,$news_id,$news_date,$news_message);
+				break;
+		}
+	}
+		$news_count++;
 }
 
 $news_xml_parser = xml_parser_create();
